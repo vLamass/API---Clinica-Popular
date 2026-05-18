04b9252c46af224d318ba7c6a48a9cf8d3746622
vLamass <viniciusslamas17@gmail.com>
2026-05-18 09:04:32 -0300

    first commit

---
+++ individual/src/test/java/org/serratec/individual/IndividualApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.individual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
